a15dd47c58a64d7b86fd99e3a93ee26da942d4f5
Saphal Patro <saphal1998@gmail.com>
2020-05-20 01:58:03 +0530

    mlh changes

--- README.md
+++ README.md
@@ -1,43 +1,20 @@
 # REST API for tests
 
+We adopt the MVC pattern and seperate out our files into routers, controllers, and models.
+
 ## Authentication Scheme
 
 1. Candidate Authentication
 2. Company Authentication
-3. Admin Authentication
+3. Admin Authentication (IN PROGRESS)
 
 ## Entities
 
 1. Candidate - The person who is using the portal
 2. Company - The company that use our portal for hiring purposes
 3. Jobs - The jobs that candidates apply for that are created by the companies
-4. Admins - Millow Admins who can control certain aspects of the test, eg. Making the standard question bank
+4. Admins - Company Admins who can control certain aspects of the test, eg. Making the standard question bank (IN PROGRESS)
 
 ## Endpoints
 
-1. Candidate
-   - Signup
-   - Signin
-   - Get All Applied jobs
-   - Apply for a particular job
-   - Check the status of his/her job application
-2. Company
-   - Create
-   - Login
-   - Get Jobs posted by Company
-   - Add question to the company database
-3. Jobs
-   - Create
-   - Delete
-   - Select Question to be asked for the job
-   - Hire a candidate for the job
-   - Submit Objective Answers
-   - Submit Subjective Answers
-4. Admin
-   - Signup
-   - Signin
-   - Add Question to standard Database
-
-## TODO
-
-[] Checking if the times add up for jobs
+Check the routers for each entity in an appropriately named files in the routers folder
--- validator/admin.validator.js
+++ validator/admin.validator.js
@@ -1,47 +1,39 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
 const AdminCreateSchema = {
-  body: {
-    email: Joi.string()
-      .required()
-      .email(),
-    name: Joi.string().required(),
-    password: Joi.string().required()
-  }
+	body: {
+		email: Joi.string().required().email(),
+		name: Joi.string().required(),
+		password: Joi.string().required(),
+	},
 };
 
 const AdminLoginSchema = {
-  body: {
-    password: Joi.string().required(),
-    email: Joi.string().email()
-  }
+	body: {
+		password: Joi.string().required(),
+		email: Joi.string().email(),
+	},
 };
 
 const AdminAddQuestionSchema = {
-  body: {
-    question: Joi.string().required(),
-    time: Joi.number()
-      .integer()
-      .required(),
-    options: Joi.string().allow(null),
-    answer: Joi.string().allow(null)
-  },
-  headers: {
-    admin_id: Joi.number()
-      .integer()
-      .required(),
-    company_id: Joi.number()
-      .integer()
-      .equal(Number(process.env.MILLOW_ID))
-      .allow(null),
-    job_type_id: Joi.number()
-      .integer()
-      .required()
-  }
+	body: {
+		question: Joi.string().required(),
+		time: Joi.number().integer().required(),
+		options: Joi.string().allow(null),
+		answer: Joi.string().allow(null),
+	},
+	headers: {
+		admin_id: Joi.number().integer().required(),
+		company_id: Joi.number()
+			.integer()
+			.equal(Number(process.env.COMPANY_ID))
+			.allow(null),
+		job_type_id: Joi.number().integer().required(),
+	},
 };
 
 module.exports = {
-  AdminCreateSchema,
-  AdminLoginSchema,
-  AdminAddQuestionSchema
+	AdminCreateSchema,
+	AdminLoginSchema,
+	AdminAddQuestionSchema,
 };
